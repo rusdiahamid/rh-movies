54fae1bc9754c780565e27abe6a84cd499e3f50e
rusdiahamid <rusdia.hamid@gmail.com>
2022-10-09 06:11:58 +0700

    a

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 const apiKey = '739949faaf1aeda8232538dbe179ea8d';
 
-fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=739949faaf1aeda8232538dbe179ea8d&language=id-ID&page=1')
-  // fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=id-ID&query=Hulk&page=1&include_adult=false`)
+fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=739949faaf1aeda8232538dbe179ea8d&language=id-ID&page=1&region=ID`)
   .then((response) => response.json())
   .then((response) => {
     const movies = response.results;
@@ -16,7 +15,8 @@ function showCard(m) {
   return `
   <div class="min-w-[160px] w-40 cursor-pointer hover:scale-110 hover:-translate-y-1 duration-300 ease-in-out">
   <img src="https://image.tmdb.org/t/p/w500${m.poster_path}" alt="" class="rounded-md bg-cover" />
-  <h5 class="text-white text-md overflow-hidden mt-3">${m.title}</h5>
-</div>
- `;
+  <span class="text-white text-sm mt-3">⭐ ${m.vote_average}</span>
+  <h5 class="text-white text-md overflow-hidden">${m.title}</h5>
+  </div>
+  `;
 }
