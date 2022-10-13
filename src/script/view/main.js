6fa2827501f12d72f41e7d49d7d959c5a8026bc3
rusdiahamid <rusdia.hamid@gmail.com>
2022-10-13 10:50:24 +0700

    tambah director & writer

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -6,8 +6,6 @@ const main = () => {
       const id = e.target.id;
       const movieDetail = await MovieSource.getMovieDetail(id);
       const movieCredits = await MovieSource.getMovieCredits(id);
-      console.log(movieDetail);
-      console.log(movieCredits);
       renderMovieDetail(movieDetail, movieCredits);
     }
   });
@@ -26,6 +24,12 @@ const main = () => {
     let minutes = Math.floor(runtime / 60);
 
     const casts = c.cast;
+    const crews = c.crew;
+    const director = crews.filter((crew) => crew.job === 'Director').map((director) => director.name);
+    const writer = crews
+      .filter((crew) => crew.job === 'Writer')
+      .map((writer) => writer.name.trim())
+      .join(' • ');
 
     return `
     <div class="container">
@@ -34,26 +38,41 @@ const main = () => {
       <p class="rating-text">⭐ <strong>${m.vote_average}</strong>/10  •  <span>${hours} Jam ${minutes} Menit</span></p>
       <div class="flex flex-col lg:flex-row">
         <div class="">
-          <img src="https://image.tmdb.org/t/p/w500${m.poster_path}" alt="${m.original_title}" class="rounded-md my-3 " />
+          <img src="https://image.tmdb.org/t/p/w500${m.poster_path}" alt="${m.original_title}" class="rounded-md my-3 mx-auto" />
         </div>
+        <div class="lg:ml-6">
         <div class="my-2">
-        ${genres.map((genre) => `<span class="text-white rounded-full bg-secondary border-white px-2 mb-3">${genre.name}</span>`.trim()).join(', ')}
-          <p class="text-white text-lg leading-relaxed mt-3 lg:ml-3">${m.overview}</p>
+        ${genres.map((genre) => `<span class="text-white rounded-full bg-secondary border-white px-2 mb-3">${genre.name}</span>`.trim()).join(' ')}
+          <p class="text-white text-lg leading-relaxed mt-3">${m.overview}</p>       
         </div>
-        <div class="flex">
-        <div class="flex min-w-[200px] gap-3 overflow-x-scroll">
-        ${casts.map(
-          (cast) => `
-          <div>          
-        <img src="https://image.tmdb.org/t/p/w500${cast.profile_path}" width="100px" class="min-w-[120px]">
-        <span class="text-white">${cast.name}</span>
+        <ul class="text-white">
+          <li class="font-bold">Director</li>
+          <li>${director}</li>
+          <li class="font-bold">Writer</li>
+          <li>${writer}</li>
+        </ul>
+        <div class="mt-3">
+          <h1 class="text-white text-2xl mb-2">Top Cast</h1>
+            <div class="flex min-w-[200px] gap-3 overflow-x-scroll">
+              ${casts
+                .slice(0, 9)
+                .map((cast) =>
+                  `
+              <div class="max-w-[120px]">          
+              <img src="https://image.tmdb.org/t/p/w500${cast.profile_path}" width="100px" class="min-w-[120px] rounded-md">
+                <figcation class="text-center">
+                <h5 class="text-white font-bold">${cast.name}</h5>
+                <h6 class="text-gray-400 text-xs">${cast.character}</h6>
+                </figcation>
+             </div>
+               `.trim()
+                )
+                .join('')}
+            </div>
         </div>
-        `
-        )}
-        </div>
-        </div>
-        </div>
-    <div>
+      </div>
+    </div>
+    </div>
         
     `;
   }
@@ -64,7 +83,6 @@ const main = () => {
   };
   const nowPlayingMovies = async () => {
     const movies = await MovieSource.getPlayingMovies();
-    console.log(movies);
     let cards = '';
     movies.forEach((m) => (cards += showCard(m)));
     const movieContainer = document.querySelector('#nowPlayingMovies');
